69533182a1adcaf3c62afc4daa1d3d0f0e2158d5
Miguel <a20190050@pucp.edu.pe>
2023-10-01 02:26:54 -0500

    más avance

--- app/src/main/java/com/example/lab4iot/AcelerometroFragment.java
+++ app/src/main/java/com/example/lab4iot/AcelerometroFragment.java
@@ -1,64 +1,79 @@
 package com.example.lab4iot;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
-
 import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link AcelerometroFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class AcelerometroFragment extends Fragment {
+public class AcelerometroFragment extends Fragment implements SensorEventListener {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private SensorManager sensorManager;
+    private Sensor accelerometer;
+    private float lastAcceleration = 0.0f;
+    private static final float SHAKE_THRESHOLD = 15.0f;
 
     public AcelerometroFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment acelerometroFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static AcelerometroFragment newInstance(String param1, String param2) {
-        AcelerometroFragment fragment = new AcelerometroFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+        // Constructor vacío requerido.
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_acelerometro, container, false);
+        View view = inflater.inflate(R.layout.fragment_acelerometro, container, false);
+
+        // Inicializar el SensorManager y el acelerómetro
+        sensorManager = (SensorManager) requireActivity().getSystemService(Context.SENSOR_SERVICE);
+        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+        return view;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        // Registrar el listener del acelerómetro
+        sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        // Detener la escucha del acelerómetro para liberar recursos
+        sensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        // No necesitas hacer nada aquí
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        // Manejar eventos de agitación
+        float acceleration = calculateAcceleration(event.values);
+
+        if (acceleration > SHAKE_THRESHOLD && lastAcceleration <= SHAKE_THRESHOLD) {
+            // Agitación detectada, muestra un Toast con la aceleración
+            String message = "Su aceleración: " + acceleration + " m/s^2";
+            Toast.makeText(requireContext(), message, Toast.LENGTH_SHORT).show();
+        }
+
+        lastAcceleration = acceleration;
+    }
+
+    private float calculateAcceleration(float values[]) {
+        return (float) Math.sqrt(values[0] * values[0] + values[1] * values[1] + values[2] * values[2]);
     }
 }
